2f3bc0e1044660c97a91b503d826c7e7bf4aa6d0
Nandan3 <69622679+Nandan3@users.noreply.github.com>
2026-03-05 19:00:56 +0530

    Refactor DynamoDB table name and response handling

--- Serverless-web-app/lambda_function.py
+++ Serverless-web-app/lambda_function.py
@@ -1,19 +1,32 @@
 import json
 import boto3
+
 dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table('serverless-web-application-on-aws')
+table = dynamodb.Table('serverless-web-app')
+
 def lambda_handler(event, context):
-    response = table.get_item(Key={
-        'id':'0'
-    })
-    views = response['Item']['views']
+
+    response = table.get_item(
+        Key={
+            'id': '0'
+        }
+    )
+
+    # If item doesn't exist, start from 0
+    if 'Item' in response:
+        views = response['Item']['views']
+    else:
+        views = 0
+
     views = views + 1
-    
+
     print(views)
-    
-    response = table.put_item(Item={
-        'id':'0',
-        'views': views
-    })
-    
+
+    table.put_item(
+        Item={
+            'id': '0',
+            'views': views
+        }
+    )
+
     return views
